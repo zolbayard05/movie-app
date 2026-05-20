844ac538ad571dc1f9879339e193ed8089223ee3
zolbayard05 <zolbayar.d05@gmail.com>
2026-05-20 20:12:44 +0800

    add api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,32 @@
+"use client";
 import { CarouselContainer } from "@/components/CarouselContainer";
 import MovieGroup from "@/components/MovieGroup";
+import axios from "axios";
 import {
   upcomingMovies,
   popularMovies,
   topRatedMovies,
 } from "@/components/MovieData";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get(
+      "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+      {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOWI4YTA3MzQ4ZGQ0YzI5NDM0ZDNjOTVmZTE4MDM1MCIsIm5iZiI6MTc3OTI3NDQyNS45OSwic3ViIjoiNmEwZDkyYjlmNGM0M2VmMTNjYjgxNWQ3Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.-P7ht59CToEV7YtGXVaI6zqc-VOe-Rwkn_x1uLA3n6I",
+        },
+      },
+    );
+    .then((response) => {
+      setMovies(response.data.results);
+    });
+  },[]);
+
   return (
     <main className="min-h-screen bg-white">
       <CarouselContainer />
